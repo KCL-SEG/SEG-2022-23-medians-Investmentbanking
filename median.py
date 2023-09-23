bf0941f18397d73cd710900e43e119a5f4ea152b
Ayesha Dorani <ayesha.dorani@kcl.ac.uk>
2023-09-23 19:22:19 +0100

    Task Completed

--- median.py
+++ median.py
@@ -1,6 +1,35 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+# Sorted values test
+# Input: 1,2,3,4,5
+# Expected output: 3.0
+# Five value test
+# Input: 8, 789, 2, 9, 391
+# Expected output: 9.0
+
+def median(numbers):
+
+    sortedList = sorted(numbers)
+    print(sortedList)
+
+    if len(sortedList) == 1:
+        return sortedList[0]
+    
+    if len(sortedList) % 2 == 0:
+        middle = int(len(sortedList) / 2)
+        #print(middle)
+        total = sortedList[middle] + sortedList[middle - 1]
+        mean = float(total / 2)
+        return float(mean)
+    else:
+        middle  = int(len(sortedList) // 2)
+        mean = sortedList[middle]
+        return float(mean)
+
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -10,3 +39,9 @@ while True:
     else:
         break
 print(numbers)
+
+medianValue = median(numbers)
+print(medianValue)
+
+
+
